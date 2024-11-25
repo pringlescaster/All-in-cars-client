c41b4230951a1d79420ca212dc1ab1a4062ded69
DAVID <pringlescaster@gmail.com>
2024-11-25 23:35:39 +0100

    production mode

--- app/Component/homeComponent/sixthSection.js
+++ app/Component/homeComponent/sixthSection.js
@@ -8,13 +8,12 @@ function SixthSection() {
   const [eventData, setEventData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const fetchEventData = async () => {
       try {
         const response = await axios.get(
-          (`${process.env.NEXT_PUBLIC_SERVER_URI_AUTH}/events/6720ac44dcec7bd43e1a0660`)
+          `${process.env.NEXT_PUBLIC_SERVER_URI_AUTH}/events/6720ac44dcec7bd43e1a0660`
         );
         setEventData([response.data]); // Wrapping in an array for mapping
         setLoading(false);
@@ -25,51 +24,21 @@ function SixthSection() {
     };
 
     fetchEventData();
-
-    const handleScroll = () => {
-      const section = document.getElementById("sixth-section");
-      const sectionTop = section.getBoundingClientRect().top;
-      const windowHeight = window.innerHeight;
-
-      if (sectionTop < windowHeight * 0.8) { // Trigger animation when section is near the bottom of the viewport
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
   }, []);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
-
   return (
-    <div
-      id="sixth-section"
-      className={`px-4 py-[60px] bg-[#030508] transition-all duration-1000 ${
-        isVisible ? "opacity-100 translateY-0" : "opacity-0 translateY-10"
-      }`}
-    >
+    <div id="sixth-section" className="px-4 py-[60px] bg-[#030508]">
       {eventData &&
         eventData.map((event, index) => (
           <div key={index} className="flex text-white flex-col gap-y-[32px]">
             <div className="px-0 md:px-8 lg:px-16 flex flex-col gap-y-[8px]">
               <h1
-                className={`font-normal text-white text-center text-lg font-russo md:text-2xl transition-all duration-1000 ${
-                  isVisible ? "opacity-100 translateY-0" : "opacity-0 translateY-10"
-                }`}
+                className="font-normal text-white text-center text-lg font-russo md:text-2xl"
               >
                 {event.title}
               </h1>
               <p
-                className={`font-montserrat text-white text-sm md:text-base font-light text-center transition-all duration-1000 ${
-                  isVisible ? "opacity-100 translateY-0" : "opacity-0 translateY-10"
-                }`}
+                className="font-montserrat text-white text-sm md:text-base font-light text-center"
               >
                 {event.description}
               </p>
@@ -77,27 +46,20 @@ function SixthSection() {
 
             <div className="flex px-0 justify-center gap-x-[16px]">
               <Image
-                className={`transition-all duration-1000 ${
-                  isVisible ? "opacity-100 translateY-0" : "opacity-0 translateY-10"
-                }`}
                 src={event.firstImage}
                 alt="eventImages"
                 width={320}
                 height={180}
               />
               <Image
-                className={`transition-all duration-1000 hidden md:block ${
-                  isVisible ? "opacity-100 translateY-0" : "opacity-0 translateY-10"
-                }`}
+                className="hidden md:block"
                 src={event.secondImage}
                 alt="eventImages"
                 width={320}
                 height={180}
               />
               <Image
-                className={`transition-all duration-1000 hidden md:hidden lg:block ${
-                  isVisible ? "opacity-100 translateY-0" : "opacity-0 translateY-10"
-                }`}
+                className="hidden md:hidden lg:block"
                 src={event.thirdImage}
                 alt="eventImages"
                 width={320}
